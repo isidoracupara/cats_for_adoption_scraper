cd8fdc913e6880fdaa5374dbdba8978c5d7eaa7f
Isidora <110285593+isidoracupara@users.noreply.github.com>
2024-12-08 20:20:10 +0100

    update github actions dependencies to latest

--- discord_webhook.py
+++ discord_webhook.py
@@ -28,7 +28,8 @@ async def check_for_new_urls():
     if new_urls:
         for url in new_urls:
             cat_name = url.split("/")[-1].replace("-", " ").title()
-            message = f""" n/
+            message = f"""
+
 ✨🐱✨ **A new cat that fits your filters has just been put up for adoption!** ✨🐱✨
 
 Meet **{cat_name}**! 🐾💖
